238f734979deebf92db1071390f4fad6664f22c2
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-03-01 20:02:40 +0500

    header input styled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import logo from '../../assets/img/linkedin.png'
 import "./header.css"
 function Header() {
   return (
     <div className='header'>
         <div className="header__left">
-            <img src="" alt="" />
+            <img src={logo} alt="" />
             <div className="header__search">
                 {/* search icon */}
+                <SearchIcon />
                 <input type="text" placeholder="Search" />
             </div>
-        </div>
+        </div> 
         <div className="header__right">
-
+             
         </div>
     </div>
   )
